1a4c44250225871dd36b3b9cb6081e436e3df4d9
Izumi Sakamoto <izu.kitty.07190719@gmail.com>
2020-02-27 13:41:01 +0900

    Initial Commit

---
+++ Trip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trip
+//
+//  Created by 坂本 泉 on 2020/02/27.
+//  Copyright © 2020 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
